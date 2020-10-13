ff6db1f9254f24a3806d7b5fe29d49b8d8d06625
Сергей Музалев <sergei.muzalev94@mail.ru>
2020-10-13 18:00:34 +0300

    block facts

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+	$('.about_facts-slider').slick({
+		// variableWidth: true,
+		slidesToScroll: 1,
+		appendArrows: $('.slider_facts-arrow'),
+		prevArrow: '<button id="prev" type="button" class="left-arrow"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>',
+        nextArrow: '<button id="next" type="button" class="right-arrow"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>'
+	});
+});

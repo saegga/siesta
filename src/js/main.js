8a77e48cb0cc3ff625ed7d030d8a18c7879a5f8b
saegga <sergei.muzalev94@mail.ru>
2020-10-14 09:09:42 +0900

    mod

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+    $('.about_facts-slider').slick({
+        // variableWidth: true,
+        slidesToScroll: 1,
+        appendArrows: $('.slider_facts-arrow'),
+        prevArrow: '<button id="prev" type="button" class="left-arrow"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>',
+        nextArrow: '<button id="next" type="button" class="right-arrow"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>'
+    });
+    $('.gallery_pubs').slick({
+        infinite: true,
+        variableWidth: true,
+        arrows: false,
+        dots: true,
+    });
+});

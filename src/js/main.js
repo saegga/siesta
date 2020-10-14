8dc10ad2d690ff99d782ee72f06d27811f8fc192
Сергей Музалев <sergei.muzalev94@mail.ru>
2020-10-14 17:59:21 +0300

    commit15

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     $('.about_facts-slider').slick({
         // variableWidth: true,
         slidesToScroll: 1,
+        slidesToShow: 1,
         appendArrows: $('.slider_facts-arrow'),
         prevArrow: '<button id="prev" type="button" class="left-arrow"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>',
         nextArrow: '<button id="next" type="button" class="right-arrow"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>'
@@ -22,4 +23,30 @@ $(document).ready(function(){
         nextArrow: '<button id="next" type="button" class="right-arrow"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>'
     });
 
+    var topSliderActive = false;
+
+    function checkTopSlider() {
+        if($(window).width() < 768){
+            if(topSliderActive === false){
+                $('.bar_item_slider').slick({
+                    dots: false,
+                    arrows: false,
+                    slidesToShow: 2,
+                    infinite: true,
+                    variableWidth: true
+                });
+                topSliderActive = true;
+            }
+        }else{
+            if(topSliderActive === true){
+                $('.bar_item_slider').slick("unslick");
+                topSliderActive = false;
+            }
+        }
+    }
+
+    checkTopSlider();
+    $(window).on('resize', function () {
+        checkTopSlider()
+    })
 });
